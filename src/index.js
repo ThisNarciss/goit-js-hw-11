868babfc64a8d81e43f3485d38457b818edf62ec
ThisNarciss <tyomaxxx000@gmail.com>
2022-12-18 11:49:07 +0200

    add dinamic mark up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const API_KEY = '32144711-033503d2fc66376fdc1e9e47c';
 const BACKEND_REF = 'https://pixabay.com/api/';
@@ -6,24 +7,50 @@ const BACKEND_REF = 'https://pixabay.com/api/';
 const formRef = document.querySelector('.search-form');
 const inputRef = document.querySelector('.search-form input');
 const btnRef = document.querySelector('.search-form button');
+const galleryRef = document.querySelector('div.gallery');
 
 console.log(formRef);
 console.log(inputRef);
 console.log(btnRef);
+console.log(galleryRef);
 
-// formRef.addEventListener('submit', onSubmitSearch);
-inputRef.addEventListener('input', onInputSearchImg);
+formRef.addEventListener('submit', onSubmitSearch);
+// inputRef.addEventListener('input', onInputSearchImg);
 
-function onInputSearchImg(evt) {
+async function onSubmitSearch(evt) {
   evt.preventDefault();
-  const inputVal = evt.target.value;
-  fetch(
+  const inputVal = evt.currentTarget.elements.searchQuery.value;
+  console.log(inputVal);
+  const response = await fetch(
     `https://pixabay.com/api/?key=32144711-033503d2fc66376fdc1e9e47c&q=${inputVal}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(response => {
-      return response.json();
+  );
+  const result = await response.json();
+  if (!result.total) {
+    Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  const imgMurkUp = result.hits
+    .map(img => {
+      return `<div class="photo-card">
+  <img src=${img.webformatURL} alt=${img.tags} loading="lazy" width="350" height="200"/>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${img.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${img.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${img.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${img.downloads}
+    </p>
+  </div>
+</div>`;
     })
-    .then(result => {
-      console.log(result);
-    });
+    .join('');
+  galleryRef.innerHTML = imgMurkUp;
+  console.log(result);
 }
